c501e4d4957ea363731896a4963694266909e8ff
mec_test_1 <mec_test_1@hotmail.com>
2015-11-17 14:35:21 +0800

    Say hello to GUI;

---
+++ HelloSwing/src/com/mec/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mec;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author MEC
+ */
+public class Main {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello, Swing!");
+	}
+	
+}
