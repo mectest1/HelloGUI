06fa024b5de52fdd612257439a694eda8337aeba
mec_test_1 <mec_test_1@hotmail.com>
2015-12-16 08:45:59 +0800

    Date Picker example;

--- HelloFX8/src/com/mec/fx/DatePickerApp.java
+++ HelloFX8/src/com/mec/fx/DatePickerApp.java
@@ -2,6 +2,12 @@ package com.mec.fx;
 
 import java.io.PrintStream;
 import java.time.LocalDate;
+import java.time.chrono.Chronology;
+import java.time.chrono.HijrahChronology;
+import java.time.chrono.IsoChronology;
+import java.time.chrono.JapaneseChronology;
+import java.time.chrono.MinguoChronology;
+import java.time.chrono.ThaiBuddhistChronology;
 import java.time.format.DateTimeFormatter;
 
 import com.mec.resources.Msg;
@@ -9,9 +15,10 @@ import com.mec.resources.Msg;
 import javafx.application.Application;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import javafx.event.Event;
+import javafx.collections.FXCollections;
 import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
@@ -53,12 +60,8 @@ public class DatePickerApp extends Application {
 			
 		});
 		
-		//
-//		StackPane root = new StackPane();
 		VBox root = new VBox(5);
 		HBox dateRow = new HBox(5);
-//		AnchorPane root = new AnchorPane();
-//		root.getChildren().add(dateRow);
 		
 		Scene scene= new Scene(root, Msg.get(this, "scene.width", Integer::parseInt, 300), Msg.get(this, "scene.height", Integer::parseInt, 300));
 		primaryStage.setScene(scene);
@@ -77,46 +80,48 @@ public class DatePickerApp extends Application {
 //		ChoiceBox formatter1 = new ChoiceBox()
 		ToggleGroup formatterGroup = new ToggleGroup();
 		formatterGroup.selectedToggleProperty().addListener((Observable, oldVal, newVal) -> {
-//			out.println(newVal);
 			dateFormatterStr.set(((RadioButton) newVal).getText());
-//			out.println(dateFormatterStr.get());
-//			datePicker.setValue(datePicker.getValue());
 			datePicker.getEditor().setText(datePicker.getConverter().toString(datePicker.getValue()));		//<- too ugly;
 		});
-//		dateFormatterStr.bind(formatterGroup.selectedToggleProperty().get);
 	
 		RadioButton formatter1 = new RadioButton(Msg.get(this, "formatter.1"));
 		formatter1.setToggleGroup(formatterGroup);
-//		formatter1.setOnAction(e -> {
-//			if(formatter1.isSelected()){
-////				dateFormatterRow.set(formatter1.getText());
-//				dateFormatterStr.set(formatter1.getText());
-//			}
-//		});
 		formatter1.setSelected(true);
 		
 		RadioButton formatter2 = new RadioButton(Msg.get(this,"formatter.2"));
 		formatter2.setToggleGroup(formatterGroup);
-//		formatter2.setOnAction(e -> {
-//			if(formatter2.isSelected()){
-////				dateFormatterRow.set(formatter1.getText());
-//				dateFormatterStr.set(formatter2.getText());
-//			}
-//		});
+		
+		//Chronology list
+		ChoiceBox<Chronology> chronologyList = new ChoiceBox<>(FXCollections.observableArrayList(
+				 IsoChronology.INSTANCE
+				,JapaneseChronology.INSTANCE
+				,HijrahChronology.INSTANCE
+				,MinguoChronology.INSTANCE
+				,ThaiBuddhistChronology.INSTANCE
+				));
+		chronologyList.setOnAction(action -> {
+			Chronology selectedChrono = chronologyList.getSelectionModel().getSelectedItem();
+			datePicker.setChronology(selectedChrono);
+//			formatterGroup.selectToggle(null);
+//			dateFormatterStr.set(selectedChrono.);
+		});
+		chronologyList.getSelectionModel().select(IsoChronology.INSTANCE);
+		HBox chronologyBar = new HBox(5);
+		chronologyBar.getChildren().add(chronologyList);
 		
 		dateFormatterRow.getChildren().addAll(formatter1, formatter2);
 		
 		
 		//
-		root.getChildren().addAll(dateFormatterRow, dateRow);
-//		dateRow.getChildren().addAll(root);
+		root.getChildren().addAll(dateFormatterRow, chronologyBar, dateRow);
+		primaryStage.sizeToScene();
 	}
 	
 	
 	
-	public static void main(String[] args) {
-		Application.launch(DatePickerApp.class);
-	}
+//	public static void main(String[] args) {
+//		Application.launch(DatePickerApp.class);
+//	}
 	
 	
 	
