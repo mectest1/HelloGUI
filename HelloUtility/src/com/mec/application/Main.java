f4464de2e60191dd9b37b5e698e4df281269d665
mec_test_1 <mec_test_1@hotmail.com>
2015-12-21 16:46:36 +0800

    Hello Utility 3000 in 1;

--- HelloUtility/src/com/mec/application/Main.java
+++ HelloUtility/src/com/mec/application/Main.java
@@ -1,10 +1,13 @@
 package com.mec.application;
 	
+import com.mec.resources.Msg;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
@@ -12,9 +15,16 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			
+			Label hello = new Label(Msg.get(this, "hello"));
+			root.setCenter(hello);
+			
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			
 			primaryStage.setScene(scene);
+			primaryStage.setTitle(Msg.get(this, "title"));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
