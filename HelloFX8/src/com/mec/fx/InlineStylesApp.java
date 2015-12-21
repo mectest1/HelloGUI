a41cab1aaa1d1c35448d9a76af18cd28861b4509
mec_test_1 <mec_test_1@hotmail.com>
2015-12-21 17:49:04 +0800

    Using styles;

---
+++ HelloFX8/src/com/mec/fx/InlineStylesApp.java
@@ -0,0 +1,13 @@
+package com.mec.fx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class InlineStylesApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+	}
+
+}
