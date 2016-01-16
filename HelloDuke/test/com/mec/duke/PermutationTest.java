57885d10176355a514553343c9d8ef3d327e3a74
mec_test_1 <mec_test_1@hotmail.com>
2016-01-16 18:55:37 +0800

    Complete permutation example;

--- HelloDuke/test/com/mec/duke/PermutationTest.java
+++ HelloDuke/test/com/mec/duke/PermutationTest.java
@@ -1,6 +1,7 @@
 package com.mec.duke;
 
 import java.io.PrintStream;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -11,7 +12,11 @@ import java.util.Set;
 import org.junit.Ignore;
 import org.junit.Test;
 
-//ref: http://www.programcreek.com/2013/02/leetcode-permutations-java/
+/**
+ * ref: ref: http://www.programcreek.com/2013/02/leetcode-permutations-java/
+ * @author MEC
+ *
+ */
 public class PermutationTest {
 
 	@Ignore
@@ -32,6 +37,298 @@ public class PermutationTest {
 	}
 	
 	
+//	@Ignore
+	@Test
+	public void trySolveHouseProblem(){
+		List<List<Color>> colors = Permutation.permutate(Arrays.asList(Color.values()));
+		List<List<Nation>> nations = Permutation.permutate(Arrays.asList(Nation.values()));
+		List<List<Beverage>> beverages = Permutation.permutate(Arrays.asList(Beverage.values()));
+		List<List<Cigar>> cigars = Permutation.permutate(Arrays.asList(Cigar.values()));
+		List<List<Pet>> pets = Permutation.permutate(Arrays.asList(Pet.values()));
+		
+		out.printf("Started on %s\n", LocalDateTime.now());
+		colors.parallelStream().forEach(color -> {
+			nations.parallelStream().forEach(nation -> {
+				beverages.parallelStream().forEach(beverage -> {
+					cigars.parallelStream().forEach(cigar -> {
+						pets.parallelStream().forEach(pet ->{
+							List<House> houses = getHouseList(color, nation, beverage, cigar, pet);
+								if(validateHouses(houses)){
+									out.println(houses);
+								}
+							
+						});
+					});
+				});
+			});
+		});
+		out.printf("Finished on %s\n", LocalDateTime.now());
+	}
+	
+	
+//	@Test
+	public static boolean validateHouses(List<House> house){
+//		boolean retval = false;
+		List<Boolean> conditions = new ArrayList<>();
+//		conditions.addAll(Arrays.asList(true, true, false));
+		//Rule 1:
+		conditions.add(house.parallelStream().anyMatch(h -> Nation.BRIT == h.getNationality() && Color.RED == h.getColor()));
+		if(!conditions.get(conditions.size() - 1)){
+			return false;
+		}
+		
+		//Rule 2:
+		conditions.add(house.parallelStream().anyMatch(h -> Nation.SWEDE == h.getNationality() && Pet.DOG == h.getPet()));
+		if(!conditions.get(conditions.size() - 1)){
+			return false;
+		}
+		
+		//Rule 3:
+		conditions.add(house.parallelStream().anyMatch(h -> Nation.DANE == h.getNationality() && Beverage.TEA == h.getBeverage()));
+		if(!conditions.get(conditions.size() - 1)){
+			return false;
+		}
+		
+		//Rule 4:
+		conditions.add(house.parallelStream().anyMatch(h -> h.getIndex() < House.HOUSE_NUMBER - 1
+				&& Color.GREEN == h.getColor()
+				&& Color.WHITE == house.get(h.getIndex() + 1).getColor()
+				));
+		if(!conditions.get(conditions.size() - 1)){
+			return false;
+		}
+		
+		//Rule 5:
+		conditions.add(house.parallelStream().anyMatch(h -> Color.GREEN == h.getColor() && Beverage.COFFEE == h.getBeverage()));
+		if(!conditions.get(conditions.size() - 1)){
+			return false;
+		}
+		
+		//Rule 6:
+		conditions.add(house.parallelStream().anyMatch(h -> Cigar.PALL_MALL == h.getCigar() && Pet.BIRD == h.getPet()));
+		if(!conditions.get(conditions.size() - 1)){
+			return false;
+		}
+		
+		//Rule 7:
+		conditions.add(house.parallelStream().anyMatch(h -> Cigar.DUNHILL == h.getCigar() && Color.YELLOW == h.getColor()));
+		if(!conditions.get(conditions.size() - 1)){
+			return false;
+		}
+		
+		//Rule 8:
+		conditions.add(Beverage.MILK == house.get(House.CENTER_POS_INDEX).getBeverage());
+		if(!conditions.get(conditions.size() - 1)){
+			return false;
+		}
+		
+		//Rule 9:
+		conditions.add(Nation.NORWEGIAN == house.get(0).getNationality());
+		if(!conditions.get(conditions.size() - 1)){
+			return false;
+		}
+		
+		//Rule 10:
+		conditions.add(house.parallelStream().anyMatch(h -> 
+				(
+						h.getIndex() < House.HOUSE_NUMBER - 1
+						&& Cigar.BLENDS == h.getCigar()
+						&& Pet.CAT == house.get(h.getIndex() + 1).getPet()
+				)
+				||
+				(
+						0 < h.getIndex()
+						&& Cigar.BLENDS == h.getCigar()
+						&& Pet.CAT == house.get(h.getIndex() - 1).getPet()
+						
+						)
+				
+				
+				));
+		if(!conditions.get(conditions.size() - 1)){
+			return false;
+		}
+		
+		//Rule 11:
+		conditions.add(house.parallelStream().anyMatch(h -> 
+				(
+						h.getIndex() < House.HOUSE_NUMBER - 1
+						&& Pet.HORSE == h.getPet()
+						&& Cigar.DUNHILL == house.get(h.getIndex() + 1).getCigar()
+				)
+				||
+				(
+						0 < h.getIndex()
+						&& Cigar.BLENDS == h.getCigar()
+						&& Cigar.DUNHILL == house.get(h.getIndex() - 1).getCigar()
+						
+						)
+				
+				
+				));
+		if(!conditions.get(conditions.size() - 1)){
+			return false;
+		}
+		
+		//Rule 12:
+		conditions.add(house.parallelStream().anyMatch(h -> Cigar.BLUEMASTER == h.getCigar() && Beverage.BEER == h.getBeverage()));
+		if(!conditions.get(conditions.size() - 1)){
+			return false;
+		}
+		
+		//Rule 13:
+		conditions.add(house.parallelStream().anyMatch(h -> Nation.GERMAN == h.getNationality() && Cigar.PRINCE == h.getCigar()));
+		if(!conditions.get(conditions.size() - 1)){
+			return false;
+		}
+		
+		//Rule 14:
+		conditions.add(house.parallelStream().anyMatch(h -> 
+				(
+						h.getIndex() < House.HOUSE_NUMBER - 1
+						&& Nation.NORWEGIAN == h.getNationality()
+						&& Color.BLUE == house.get(h.getIndex() + 1).getColor()
+				)
+				||
+				(
+					0 < h.getIndex()
+					&& Nation.NORWEGIAN == h.getNationality()
+					&& Color.BLUE == house.get(h.getIndex() -1).getColor()
+				)
+				));
+		
+		if(!conditions.get(conditions.size() - 1)){
+			return false;
+		}
+		//Rule 15:
+		conditions.add(house.parallelStream().anyMatch(h -> 
+				(
+						h.getIndex() < House.HOUSE_NUMBER - 1
+						&& Cigar.BLENDS == h.getCigar()
+						&& Beverage.WATER == house.get(h.getIndex() + 1).getBeverage()
+				)
+				||
+				(
+					0 < h.getIndex()
+					&& Cigar.BLENDS == h.getCigar()
+					&& Beverage.WATER == house.get(h.getIndex() -1).getBeverage()
+				)
+				));
+		if(!conditions.get(conditions.size() - 1)){
+			return false;
+		}
+		
+//		boolean retval = conditions.stream().reduce((l, r) -> l && r).get();
+//		boolean retval = true;
+//		out.printf("Conditions: %s, result: %s\n", conditions, retval);
+//		return retval;
+		return true;
+	}
+	
+	
+	
+	
+//	@Test
+//	public void testFilterList(){
+//		List<List<Color>> colors = Permutation.permutate(Arrays.asList(Color.values()));
+//		List<List<Nation>> nations = Permutation.permutate(Arrays.asList(Nation.values()));
+//		
+//		Tuple<List<List<Color>>, List<List<Nation>>> result = filterList(colors, Color.RED, nations, Nation.BRIT);
+//		List<List<Color>> colorsR = result.getObj1();
+//		List<List<Nation>> nationsR = result.getObj2();
+//		colorsR.forEach(out::println);
+//		out.println();
+//		nationsR.forEach(out::println);
+//	}
+	
+	
+//	/**
+//	 * Given a two collections of list, with <strong>any</strong> two list in them have the <strong>same</strong> index 
+//	 * for <code>matcher1</code>|<code>match2</code>, then put them in the returned list. 
+//	 * @param listCollection1
+//	 * @param match1
+//	 * @param listCollection2
+//	 * @param match2
+//	 * @return
+//	 */
+//	<T1, T2> Tuple<List<List<T1>>, List<List<T2>>> filterList(List<List<T1>> listCollection1, T1 match1, List<List<T2>> listCollection2, T2 match2){
+//
+//		Set<List<T1>> retList1 = new HashSet<>();
+//		Set<List<T2>> retList2 = new HashSet<>();
+//		
+//		for(int i = 0; i < listCollection1.size(); ++i){
+//			for(int j = 0; j < listCollection2.size(); ++j){
+//				List<T1> list1 = listCollection1.get(i);
+//				List<T2> list2 = listCollection2.get(j);
+//				
+//				if(matchPosition(list1, match1, list2, match2)){
+//					retList1.add(list1);
+//					retList2.add(list2);
+//				}
+//			}
+//		}
+//		
+//		return new Tuple<>(new ArrayList<>(retList1), new ArrayList<>(retList2));
+//	}
+//	
+	@FunctionalInterface
+	static interface PositionFilter{
+		<T1, T2> boolean matchPosition(List<T1> list1, T1 match1, List<T2> list2, T2 match2);
+	}
+	/**
+	 * Check of <code>match1</code> in <code>list1</code> bears the same position with <code>match2</code> in <code>list2</code>
+	 * @param list1
+	 * @param list2
+	 * @param match1
+	 * @param match2
+	 */
+	<T1, T2> boolean matchPosition(List<T1> list1, T1 match1, List<T2> list2, T2 match2){
+
+		
+		boolean retval = false;
+
+		if(list1.indexOf(match1) == list2.indexOf(match2)){
+			retval = true;
+		}
+		return retval;
+	}
+	
+	/**
+	 * Return the house list the specific order: from 0 to {@link House#HOUSE_NUMBER}. Characteristics for each house are given
+	 * through the parameters listed here.
+	 * @param colors colors for each house
+	 * @param nations nationality of each host of each house
+	 * @param beverages favourite beverage of each host   
+	 * @param cigars favourite cigar of each host
+	 * @param pets pets for each house
+	 * @return
+	 */
+	List<House> getHouseList(List<Color> colors, List<Nation> nations, List<Beverage> beverages, List<Cigar> cigars, List<Pet> pets){
+		List<House> retval = new ArrayList<>();
+		
+		for(int index = 0; index < House.HOUSE_NUMBER; ++index){
+			retval.add(new House(index, colors.get(index), nations.get(index), beverages.get(index), cigars.get(index), pets.get(index)));
+		}
+		
+		return retval;
+	}
+	
+	
+	static class Tuple<T1, T2>{
+		T1 obj1;
+		T2 obj2;
+		public Tuple(T1 obj1, T2 obj2) {
+			super();
+			this.obj1 = obj1;
+			this.obj2 = obj2;
+		}
+		public T1 getObj1() {
+			return obj1;
+		}
+		public T2 getObj2() {
+			return obj2;
+		}
+	}
 	static class Permutation{
 		/**
 		 * Input the original item list, list all possible permutations.
@@ -93,7 +390,12 @@ public class PermutationTest {
 	
 	
 	static class House{
-		static final int NUMBER = 5;
+		/**
+		 * Total number of houses
+		 */
+		public static final int HOUSE_NUMBER = 5;
+		public static final int CENTER_POS_INDEX = 2;
+		
 		int index;	//Starts from 0
 		Color color;
 		Nation nationality;
@@ -107,6 +409,29 @@ public class PermutationTest {
 			this.beverage = beverage;
 			this.cigar = cigar;
 			this.pet = pet;
+		}
+		public int getIndex() {
+			return index;
+		}
+		public Color getColor() {
+			return color;
+		}
+		public Nation getNationality() {
+			return nationality;
+		}
+		public Beverage getBeverage() {
+			return beverage;
+		}
+		public Cigar getCigar() {
+			return cigar;
+		}
+		public Pet getPet() {
+			return pet;
+		}
+		@Override
+		public String toString() {
+			return "House [index=" + index + ", color=" + color + ", nationality=" + nationality + ", beverage="
+					+ beverage + ", cigar=" + cigar + ", pet=" + pet + "]";
 		}
 	}
 	
