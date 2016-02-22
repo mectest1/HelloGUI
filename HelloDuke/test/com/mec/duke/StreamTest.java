e743856ad4ba32a438066759e237c3863b411c1f
mec_test_1 <mec_test_1@hotmail.com>
2016-02-22 16:20:24 +0800

    Working with more stream examples;

--- HelloDuke/test/com/mec/duke/StreamTest.java
+++ HelloDuke/test/com/mec/duke/StreamTest.java
@@ -1,6 +1,7 @@
 package com.mec.duke;
 
 import java.io.PrintStream;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,6 +11,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.function.IntBinaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
@@ -257,20 +259,245 @@ public class StreamTest {
 	
 	
 
+	@Ignore
 	@Test
 	public void testFlatMap2(){
 		List<Integer> numbers1 = Arrays.asList(1, 2, 3);
 		List<Integer> numbers2 = Arrays.asList(4, 5);
 //		numbers1.stream().map(i -> {
 		numbers1.stream().flatMap(i -> {
-			return numbers2.stream().map(j -> new int[]{i, j});
+			return numbers2.stream()
+					.filter(j -> 0 == (i + j) % 3)	//<- filter specific elements combination
+					.map(j -> new int[]{i, j});
 		})
 		.forEach(pair -> out.printf("%s, %s\n", pair[0], pair[1]));
 		;
 	}
 	
+	@Ignore
+	@Test
+	public void testAnyMatch(){
+		if(menus().anyMatch(Dish::isVegetarian)){
+			out.println("The menu is (somewhat) vegetarian friendly");
+		}
+	}
+	
+	@Ignore
+	@Test
+	public void testAllMathes(){
+		if(menus().allMatch(d -> d.getCalories() < 1000)){
+			out.println("Calories of all dishes are under 1000");
+		}
+	}
+	
+	@Ignore
+	@Test
+	public void testNoneMatch(){
+		if(menus().noneMatch(d -> d.getCalories() > 1000)){	//<- Complementary 
+			out.println("No calories of dish is over 1000");
+		}
+	}
+	
+	@Ignore
+	@Test
+	public void testFindAny(){
+		menus().filter(Dish::isVegetarian).findAny().ifPresent(out::println);
+	}
+	@Ignore
+	@Test
+	public void testFindFirst(){
+		menus().findFirst().ifPresent(out::println);
+	}
+	
+	@Ignore
+	@Test
+	public void testReduce(){
+		IntBinaryOperator sum = (x, y) -> x + y;
+		IntStream.rangeClosed(1, 100).reduce(sum).ifPresent(out::println);
+//		LongBinaryOperator multiply = (x, y) -> 
+//			x * y;
+//		LongStream.rangeClosed(1, 100).reduce(multiply).ifPresent(out::println);	//<- the result is 0; Why? it has overflown around 70
+		
+//		LongStream.rangeClosed(1, 100).reduce((x, y) -> {
+//			BigInteger xl = BigInteger.valueOf(x);
+//			BigInteger yl = BigInteger.valueOf(y);
+//			
+//		});
+		
+		LongStream.rangeClosed(1, 100).forEach(x -> {
+//			BigInteger xl = BigInteger.valueOf(x);
+			longMultiply = longMultiply.multiply(BigInteger.valueOf(x));
+		});
+		out.printf("Multiple from 1 to 100 gives %s\n", longMultiply.toString());
+		
+		IntStream.rangeClosed(1, 100).boxed().map(BigInteger::valueOf).reduce(BigInteger::multiply).ifPresent(out::println);	//<- more concise way to calculate multiplication;
+		
+		IntStream.rangeClosed(1, 100).reduce(Integer::sum).ifPresent(out::println);
+		out.println(IntStream.rangeClosed(1, 100).sum());
+	}
+	
+	BigInteger longMultiply = BigInteger.ONE;
+	
+	@Ignore
+	@Test
+	public void testExtractMaxMin(){
+		Random rand = new Random();
+		List<Integer> ints = IntStream.generate(() -> rand.nextInt()).limit(10).boxed().collect(Collectors.toList());
+		ints.stream().reduce(Integer::max).ifPresent(out::println);
+		ints.stream().reduce(Integer::min).ifPresent(out::println);	//<- Stream has already been operated upon or closed;
+	}
 	
 	
+	@Ignore
+	@Test
+	public void testCountElements(){
+		menus().map(d -> 1).reduce(Integer::sum).ifPresent(out::println);
+		out.printf("Total number of dishes: %s\n", menus().count());
+	}
+	
+	@Ignore
+	@Test
+	public void testLimitedSort(){
+		Random rand = new Random();
+		IntStream.generate(() -> rand.nextInt()).limit(10).sorted().forEach(out::print);	//<- limit first, sort next;
+		IntStream.generate(() -> rand.nextInt()).sorted().limit(10).forEach(out::print);	//<- sort before limit: this code will keep running;
+	}
+	
+	@Ignore
+	@Test
+	public void testDistinct(){
+		Random rand = new Random();
+		IntStream.generate(() -> rand.nextInt(10)).limit(20).distinct().sorted().forEach(out::println);
+	}
+	
+	@Ignore
+	@Test
+	public void testTransactions(){
+		List<Transaction> transactions = transactions();
+		//1, Find all transactions in the year 2011 and sort them by value;
+		transactions.stream().filter(t -> 2011 == t.getYear()).sorted(Comparator.comparing(Transaction::getValue)).forEach(out::println);
+		
+		//2, What are all the unique cities where the traders work
+		transactions.stream().map(Transaction::getTrader).map(Trader::getCity).distinct().forEach(out::println);
+		//or:
+		transactions.stream().map(t -> t.getTrader().getCity()).distinct().forEach(out::println);
+		//or
+		transactions.stream().map(t -> t.getTrader().getCity()).collect(Collectors.toSet()).stream().forEach(out::println);
+		
+		//3, Find all traders from Cambridge and sort them by name;
+		transactions.stream().map(Transaction::getTrader).filter(t -> "Cambridge".equals(t.getCity())).distinct().sorted(Comparator.comparing(Trader::getName)).forEach(out::println);
+		
+		//4, Return a string of all traders' names sorted alphabetically
+		transactions.stream().map(Transaction::getTrader).distinct().map(Trader::getName).sorted().forEach(out::println);
+		//or: 
+		out.println(transactions.stream().map(t -> t.getTrader().getName()).distinct().sorted()
+//			.reduce(String::concat);
+//			.reduce("", (l, r) -> l + ", " + r) //or format the result better;
+			.collect(Collectors.joining(", "))
+				);	
+		
+		
+		//5, Are any traders based in Milan
+		if(transactions.stream().map(Transaction::getTrader).anyMatch(t -> "Milan".equals(t.getCity()))){
+			out.println("Yes, there is at least one trader based in Milan.");
+		};
+		//or:
+		transactions.stream().anyMatch(t -> "Milan".equals(t.getTrader().getCity()));
+		
+		//6, Print all transaction's values from the traders living in Cambridge
+		transactions.stream().filter(t -> "Cambridge".equals(t.getTrader().getCity())).map(Transaction::getValue).forEach(out::println);
+		
+		//7, What's highest value of all the transactions
+		transactions.stream().map(Transaction::getValue).reduce(Integer::max).ifPresent(out::println);
+		//or:
+		transactions.stream().max(Comparator.comparing(Transaction::getValue)).ifPresent(t -> out.println(t.getValue()));
+		
+		//8, Find the transaction with the smallest value
+//		transactions.stream().map(Transaction::getValue).reduce(Integer::min).ifPresent(out::println);	/It is the transaction record we needed here
+		transactions.stream().reduce((t1, t2) -> t1.getValue() < t2.getValue() ? t1 : t2).ifPresent(out::println);	//<- this is the record we want here;
+		transactions.stream().min(Comparator.comparing(Transaction::getValue)).ifPresent(out::println);
+	}
+	
+	@Ignore
+	@Test
+	public void testFibonacci2(){
+		Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]}).map(t -> t[0])
+			.limit(10).forEach(out::println);
+	}
+	
+
+	//------------------------------
+	//---Domain: Transactions-------
+	//------------------------------
+	List<Transaction> transactions(){
+		Trader raoul = new Trader("Raoul", "Cambridge");
+		Trader mario = new Trader("Mario", "Milan");
+		Trader alan = new Trader("Alan", "Cambridge");
+		Trader brian = new Trader("Brian", "Cambridge");
+		List<Transaction> transactions = Arrays.asList(
+			new Transaction(brian, 2011, 300),
+			new Transaction(raoul, 2012, 1000),
+			new Transaction(raoul, 2011, 400),
+			new Transaction(mario, 2012, 710),
+			new Transaction(mario, 2012, 700),
+			new Transaction(alan, 2012, 950)
+		);
+		return transactions;
+	}
+	
+	class Trader{
+		final String name;
+		final String city;
+		public Trader(String name, String city) {
+			super();
+			this.name = name;
+			this.city = city;
+		}
+		public String getName() {
+			return name;
+		}
+		public String getCity() {
+			return city;
+		}
+		@Override
+		public String toString() {
+			return "Trader [name=" + name + ", city=" + city + "]";
+		}
+	}
+	class Transaction{
+		final Trader trader;
+		final int year;
+		final int value;
+		public Transaction(Trader trader, int year, int value) {
+			super();
+			this.trader = trader;
+			this.year = year;
+			this.value = value;
+		}
+		public Trader getTrader() {
+			return trader;
+		}
+		public int getYear() {
+			return year;
+		}
+		public int getValue() {
+			return value;
+		}
+		@Override
+		public String toString() {
+			return "Transaction [trader=" + trader + ", year=" + year + ", value=" + value + "]";
+		}
+	}
+	
+	//-----------------------
+	
+	
+	@Test
+	public void testPrimtiveStream(){
+		
+		
+		
+	}
 	
 	
 	
@@ -278,6 +505,7 @@ public class StreamTest {
 	private static final PrintStream out = System.out;
 	
 	class FibonacciGenerator{
+		@Deprecated
 		Stream<Integer> fibonacci(){
 			final int seed = 1;
 			prev2 = 0;
@@ -293,6 +521,12 @@ public class StreamTest {
 		}
 		
 		int prev2;
+		
+		
+		//
+		Stream<Integer> fibonacci2(){
+			return Stream.iterate(new int[]{0, 1}, t -> new int[]{t[1], t[0] + t[1]}).map(t -> t[0]);
+		}
 	}
 	
 	
