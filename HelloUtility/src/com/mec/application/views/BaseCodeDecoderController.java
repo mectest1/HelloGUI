d6ca11735158879327d2ad00fdeab093d35d881c
mec_test_1 <mec_test_1@hotmail.com>
2016-03-16 17:35:38 +0800

    Decode OpenWin_Para;

--- HelloUtility/src/com/mec/application/views/BaseCodeDecoderController.java
+++ HelloUtility/src/com/mec/application/views/BaseCodeDecoderController.java
@@ -7,6 +7,8 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.nio.charset.Charset;
 import java.util.Base64;
 import java.util.Optional;
@@ -16,8 +18,10 @@ import java.util.zip.GZIPOutputStream;
 
 import javax.crypto.Cipher;
 
+import com.mec.resources.JarTool;
 import com.mec.resources.Msg;
 import com.mec.resources.ViewFactory;
+import com.sun.crypto.provider.DHKeyAgreement;
 
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
@@ -79,14 +83,20 @@ public class BaseCodeDecoderController {
 	@FXML
 	private void onExtraBase64ToStringUTF8(){
 //		decodeBase64ToStringDirectly(Msg.get(this, "charset.default"));
-		proceedDecode(s -> decodeBase64ToStringDirectly(s, Msg.get(this, "charset.default")));
+		proceedDecode(s -> decodeBase64ToStringDirectly(s, CHARSET_DEFAULT));
 	}
 	
 	@FXML
 	private void onExtraBase64ToString(){
-		Optional<String> charSetStr = ViewFactory.inputText(Msg.get(this, "charset.default"), Msg.get(this, "charset.new.title"), Msg.get(this, "charset.new.header")); 
+		Optional<String> charSetStr = ViewFactory.inputText(CHARSET_DEFAULT, Msg.get(this, "charset.new.title"), Msg.get(this, "charset.new.header")); 
 //		decodeBase64ToStringDirectly(charSetStr.get());
 		charSetStr.ifPresent(charset -> proceedDecode(s -> decodeBase64ToStringDirectly(s, charset)));
+	}
+	
+	@FXML
+	private void onExtraDecodeOpenWinPara(){
+		String charsetName = Msg.get(this, "openWinPara.charset");
+		proceedDecode(s -> decodeOpenWinPara(s, charsetName));
 	}
 
 	@FXML
@@ -152,7 +162,9 @@ public class BaseCodeDecoderController {
 		
 		onPrepare(decodedText);
 		try {
-			decodeFunc.apply(encodedStr).ifPresent(str -> decodedText.setText(str));
+			Optional<String> decodedStr = decodeFunc.apply(encodedStr);
+			decodedStr.ifPresent(str -> decodedText.setText(str));
+//			decodedStr.
 		} catch (Exception e) {
 			String errorInfo = String.format(ERROR_OUTPUT, e.getClass().getName(), e.getMessage());	//<- Should be generalized;
 			onError(decodedText, errorInfo);
@@ -219,6 +231,28 @@ public class BaseCodeDecoderController {
 		return Optional.of(retval);
 	}
 	
+	private static Optional<String> decodeOpenWinPara(String encodedStr, String charsetName){
+//		Charset charset = Charset.forName(charsetName);
+		if(null == encodedStr){
+			return Optional.empty();
+		}
+		String retval = null;
+		//----extracted from Base64Decoder
+		try{
+			byte[] bytes =  encodedStr.getBytes(charsetName);
+			bytes = Base64.getMimeDecoder().decode(bytes);
+			retval = new String(bytes, charsetName);
+			retval = URLDecoder.decode(retval, CHARSET_DEFAULT);
+			retval = retval.replaceAll(Msg.get(BaseCodeDecoderController.class, "openWinPara.url.paraDelimiter")
+					, Msg.get(BaseCodeDecoderController.class, "openWinPara.url.replaceDelimiter"));
+		}catch(UnsupportedEncodingException e){
+			throw new RuntimeException(e);
+//			retval = JarTool.exceptionToStr(e);
+		}
+		
+		return Optional.of(retval);
+	}
+	
 	public static Optional<String> encodeBase64(Object decodedObj, boolean isGZipped){
 		try {
 			if (null == decodedObj) {
@@ -244,4 +278,5 @@ public class BaseCodeDecoderController {
 //	private static final String CIPHER_TRANSFORM_DES = "DESede";
 	private static final String ERROR_OUTPUT = Msg.get(BaseCodeDecoderController.class, "decode.error.message");
 	private static final String ERROR_STYLE = Msg.get(BaseCodeDecoderController.class, "errorStyle"); 
+	private static final String CHARSET_DEFAULT = Msg.get(BaseCodeDecoderController.class, "charset.default");
 }
