8478371059fa557113ad3e0b4a8f61a684727ab9
mec_test_1 <mec_test_1@hotmail.com>
2016-07-06 10:31:17 +0800

    XML Transformation test;

---
+++ HelloDuke/test/com/mec/duke/xml/TransformerTest.java
@@ -0,0 +1,19 @@
+package com.mec.duke.xml;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class TransformerTest {
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
